447d5f72c724ae389709e117c019df8e75905494
Ana Ozuna <55596313+AnaOzuna01@users.noreply.github.com>
2019-10-30 19:06:21 -0400

    Proyecto actualizado. En esta ocacion hemos realizado el CRUD de nuestro proyecto.

--- Desktop/my-ice-app/src/routes/index.js
+++ Desktop/my-ice-app/src/routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Task = require('../models/task');
 
 
-router.get('/', (req, res) => {
-    res.send('Hello yo');
+//get a list taks from the db
+router.get('/', async (req, res) => {
+    console.log('Select');
+}); 
+
+router.post('/add', async (req, res) =>{
+    console.log('Insert');
+});
+
+
+router.get('return:/id', async (req, res) =>{
+    console.log('Update');
+});
+
+router.get('delete:/id', async (req, res) =>{
+    console.log('Delete');
 });
 module.exports = router; 
